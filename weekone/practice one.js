b4c866fed0081c559d778ab80d69fb53470bb0d2
陆一凡 <luyifan@luyifans-MacBook-Air.local>
2017-06-06 23:12:51 -0400

    first commit

---
+++ weekone/practice one.js
@@ -0,0 +1,11 @@
+const delayed = function ( ) {
+    setTimeout(
+        function () {
+            let x = 42
+            return x
+        }, 2000)
+}
+console.log('starting run')
+const x = delayed()
+console.log('x is now', x)
+console.log('ending run')
